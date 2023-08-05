138a0fa0dc9aed5e63d85c3c6a50886327e8d843
Zeeshan Khalid <zeeshanthedev@gmail.com>
2023-08-05 18:08:41 +0500

    made a js parser for zosl please make sure to contribute to zosl by writing a parser for a programming language of your choice

--- parser.js
+++ parser.js
@@ -1,35 +1,46 @@
 const fs = require("fs");
 
+// Function to remove comments from the input
+function removeComments(input) {
+  return input.replace(/\/\/.*$/gm, "");
+}
+
 // Function to tokenize the input based on the provided ABNF grammar
 function tokenize(input) {
+  const lines = input.split(/\r\n|\r|\n/);
   const tokens = [];
   const groups = {};
   const relationships = [];
-  const tokenRegex =
-    /(@[\w-]+(?:\([^)]*\))? *\[([^\]]*)\]|\[([^\]]*)\]|\[[^\]]*\]|@[\w-]+(?:\([^)]*\))? *-> *@[\w-]+(?:\([^)]*\))?|@[\w-]+|\S+)/g;
+  const tokenRegex = /(@[\w-]+(?:\([^)]*\))?| *\[.*?\]| *-> *)/g;
 
   let currentGroup;
-  let match;
-  while ((match = tokenRegex.exec(input)) !== null) {
-    const token = match[0].trim();
-    tokens.push(token);
+  for (const line of lines) {
+    const trimmedLine = line.trim();
+    if (!trimmedLine) {
+      continue; // Skip empty lines
+    }
 
-    if (token.startsWith("@")) {
+    if (trimmedLine.startsWith("@")) {
       // If it's a group or relationship, set it as the current group or add it to relationships
-      if (token.includes("->")) {
-        relationships.push(token);
+      if (trimmedLine.includes("->")) {
+        relationships.push(trimmedLine);
       } else {
-        currentGroup = token.replace(/\(.*\)/, "").slice(1);
+        currentGroup = trimmedLine.replace(/\(.*\)/, "").slice(1);
         groups[currentGroup] = { fields: [] };
       }
-    } else if (token === "end" + currentGroup) {
+    } else if (trimmedLine === "$end" + currentGroup) {
       // If it's the end of a group, clear the currentGroup
       currentGroup = undefined;
-    } else if (token.startsWith("[") && currentGroup) {
+    } else if (trimmedLine.startsWith("[")) {
       // If it's a field, add it to the current group's fields
-      const fieldContent = token.slice(1, -1).trim();
-      groups[currentGroup].fields.push(fieldContent);
+      const fieldContent = trimmedLine.slice(1, -1).trim();
+      // Skip adding commented fields to the group's fields
+      if (!fieldContent.startsWith("//")) {
+        groups[currentGroup].fields.push(fieldContent);
+      }
     }
+
+    tokens.push(trimmedLine);
   }
 
   return { tokens, groups, relationships };
@@ -41,20 +52,9 @@ function readInputFile(inputFilePath, callback) {
     if (err) {
       console.error("Error reading the input file:", err.message);
     } else {
-      callback(inputContent);
+      callback(removeComments(inputContent)); // Remove comments before tokenizing
     }
   });
 }
 
-// Usage example:
-const inputFile = "example.zosl"; // Replace with the actual path to your input file
-// const abnfFilePath = "zosl.abnf"; // Replace with the actual path to your ABNF file
-
-// Read the input content from the file
-readInputFile(inputFile, (inputContent) => {
-  // Tokenize the input based on the ABNF grammar
-  const result = tokenize(inputContent);
-  console.log("Tokens:", result.tokens);
-  console.log("Groups:", result.groups);
-  console.log("Relationships:", result.relationships);
-});
+module.exports = { readInputFile, tokenize };
